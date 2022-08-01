0722a6e6863e5429a54a7de4defd97b785877f15
Thales Menezes <thlmenezes@pm.me>
2022-08-01 01:57:32 -0300

    ✨ naive attack impl

--- ataque.py
+++ ataque.py
@@ -10,63 +10,64 @@
 from cifra import cifrador, decifrador
 import string
 from collections import Counter
+import itertools
 
 pt_BR_freq = {
-    "a": 14.63,
-    "b": 1.04,
-    "c": 3.88,
-    "d": 4.99,
-    "e": 12.57,
-    "f": 1.02,
-    "g": 1.30,
-    "h": 1.28,
-    "i": 6.18,
-    "j": 0.40,
-    "k": 0.02,
-    "l": 2.78,
-    "m": 4.74,
-    "n": 5.05,
-    "o": 10.73,
-    "p": 2.52,
-    "q": 1.20,
-    "r": 6.53,
-    "s": 7.81,
-    "t": 4.34,
-    "u": 4.63,
-    "v": 1.67,
-    "w": 0.01,
-    "x": 0.21,
-    "y": 0.01,
-    "z": 0.47,
+    "a": 0.1463,
+    "b": 0.0104,
+    "c": 0.0388,
+    "d": 0.0499,
+    "e": 0.1257,
+    "f": 0.0102,
+    "g": 0.0130,
+    "h": 0.0128,
+    "i": 0.0618,
+    "j": 0.0040,
+    "k": 0.0002,
+    "l": 0.0278,
+    "m": 0.0474,
+    "n": 0.0505,
+    "o": 0.1073,
+    "p": 0.0252,
+    "q": 0.0120,
+    "r": 0.0653,
+    "s": 0.0781,
+    "t": 0.0434,
+    "u": 0.0463,
+    "v": 0.0167,
+    "w": 0.0001,
+    "x": 0.0021,
+    "y": 0.0001,
+    "z": 0.0047,
 }
 
 en_US_freq = {
-    "a": 8.167,
-    "b": 1.492,
-    "c": 2.782,
-    "d": 4.253,
-    "e": 12.702,
-    "f": 2.228,
-    "g": 2.015,
-    "h": 6.094,
-    "i": 6.966,
-    "j": 0.153,
-    "k": 0.772,
-    "l": 4.025,
-    "m": 2.406,
-    "n": 6.749,
-    "o": 7.507,
-    "p": 1.929,
-    "q": 0.095,
-    "r": 5.987,
-    "s": 6.327,
-    "t": 9.056,
-    "u": 2.758,
-    "v": 0.978,
-    "w": 2.360,
-    "x": 0.150,
-    "y": 1.974,
-    "z": 0.074,
+    "a": 0.0749,
+    "b": 0.0129,
+    "c": 0.0354,
+    "d": 0.0362,
+    "e": 0.1400,
+    "f": 0.0218,
+    "g": 0.0174,
+    "h": 0.0422,
+    "i": 0.0665,
+    "j": 0.0027,
+    "k": 0.0047,
+    "l": 0.0357,
+    "m": 0.0339,
+    "n": 0.0674,
+    "o": 0.0737,
+    "p": 0.0243,
+    "q": 0.0026,
+    "r": 0.0614,
+    "s": 0.0695,
+    "t": 0.0985,
+    "u": 0.0300,
+    "v": 0.0116,
+    "w": 0.0169,
+    "x": 0.0028,
+    "y": 0.0164,
+    "z": 0.0004,
 }
 
 
@@ -126,19 +127,77 @@ def compara_frequencia(criptograma: str, lingua: dict) -> float:
     return absolute
 
 
+def freq_decifra(
+    criptograma: str,
+    lingua: dict,
+    min_tamanho_chave: int = None,
+    max_tamanho_chave: int = None,
+):
+    cifrado = [c for c in criptograma.lower() if c in string.ascii_lowercase]
+    max_tamanho_chave = max_tamanho_chave or 20
+    min_tamanho_chave = min_tamanho_chave or 1
+    chaves = []
+
+    for comprimento in range(min_tamanho_chave, max_tamanho_chave):
+        # testa todos os comprimentos de chaves dentro do intervalo selecionado
+        chave = comprimento * [None]
+        for posicao_da_senha in range(comprimento):
+            proximidade = []
+            # seleciona do criptograma as letras da mensagem que foram cifradas
+            # com a letra atual da chave (top_key[i])
+            # comprimento 1 -> [100%]
+            # comprimento 2 -> [50%,50%] ...
+            # usa itertools para evitar a instanciação do slice como lista
+            alfabeto = "".join(
+                itertools.islice(cifrado, posicao_da_senha, None, comprimento)
+            )
+            for letra in string.ascii_lowercase:
+                # Calcula o grau de proximidade com a lingua inglesa para o resultado
+                # da decifração utilizando cada uma das letras do alfabeto
+                proximidade.append(
+                    (
+                        compara_frequencia(decifrador(letra, alfabeto), lingua),
+                        letra,
+                    )
+                )
+            # a melhor escolha é a que possui o menor delta em relação a lingua inglesa
+            # ou seja, gerou o melhor texto decifrado
+            chave[posicao_da_senha] = min(proximidade, key=lambda z: z[0])[1]
+        # salva a chave gerada da iteração atual
+        chaves.append("".join(chave))
+    # ordena todas as chaves geradas pela capacidade de
+    # gerar mensagens próximas a lingua
+    chaves.sort(
+        key=lambda senha: compara_frequencia(decifrador(senha, criptograma), lingua)
+    )
+    # cria uma cópia do primeiro da lista
+    return chaves[:1]
+
+
 if __name__ == "__main__":
     pt_BR = cifrador(
         "manuel",
         """
     A cifra de Vigenère é um método de criptografia que
     usa uma série de diferentes cifras de César baseadas
-    em letras de uma senha. Originalmente descrita
-    por Giovan Battista Bellaso no seu livro datado de 1553,
-    trata-se de uma versão simplificada de uma mais geral
-    cifra de substituição polialfabética, inventada por
-    Leon Battista Alberti cerca de 1465.
+    em letras de uma senha. A invenção da cifra de Vigenère
+    é erradamente atribuída a Blaise de Vigenère encontra-se
+    originalmente descrita por Giovan Battista Bellaso no
+    seu livro datado de 1553 com o título La Cifra del Sig.
+    Giovan Battista Bellaso. Esta cifra é muito conhecida
+    porque é fácil de perceber e de pôr em prática,
+    parecendo, a quem tem pouca prática, que é inquebrável
+    (indecifrável). Consequentemente, muitos programadores
+    implementaram esquemas de criptografia nas suas aplicações
+    que são no essencial cifras de Vigenère, e que são
+    facilmente quebradas por qualquer criptoanalista.
         """,
     )
+
+    for k in reversed(freq_decifra(pt_BR, pt_BR_freq)):
+        print("")
+        print("Decypted key: {!r}".format(k))
+        print("Decypted Message:", decifrador(k, pt_BR))
 
     en_US = cifrador(
         "manual",
@@ -153,3 +212,8 @@ if __name__ == "__main__":
     centuries later.
         """,
     )
+
+    for k in reversed(freq_decifra(en_US, en_US_freq)):
+        print("")
+        print("Decypted key: {!r}".format(k))
+        print("Decypted Message:", decifrador(k, en_US))
